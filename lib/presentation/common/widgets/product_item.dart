c22c7633a4410cfe9e1d96576ec599638970d63a
Apiwat Aeeabundit <apiwat.aee@innovasive.co.th>
2024-05-12 22:39:54 +0700

    🏔 refactor: move inner widget to common

--- lib/presentation/common/widgets/product_item.dart
+++ lib/presentation/common/widgets/product_item.dart
@@ -1,12 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-import '../../../domain/models/product_model.dart';
-import '../../../domain/states/saved_product_list_provider_state_provider.dart';
+import '../../product/pages/product_detail_page.dart';
+import 'saved_button.dart';
 
 class ProductItem extends StatelessWidget {
   const ProductItem({
@@ -51,7 +48,7 @@ class ProductItem extends StatelessWidget {
                 Positioned(
                   top: 8,
                   right: 8,
-                  child: _SaveButton(
+                  child: SaveButton(
                     id: id,
                     productName: productName,
                     imageUrl: imageUrl,
@@ -80,52 +77,6 @@ class ProductItem extends StatelessWidget {
             ),
           )
         ],
-      ),
-    );
-  }
-}
-
-class _SaveButton extends ConsumerWidget {
-  const _SaveButton({
-    required this.id,
-    required this.productName,
-    required this.imageUrl,
-    required this.price,
-  });
-
-  final String id;
-  final String productName;
-  final String imageUrl;
-  final double price;
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final isFavorite = ref.watch(savedProductListStateProvider).indexWhere((e) => e.id == id) > -1;
-
-    return CupertinoButton(
-      onPressed: () {
-        final indexOfProduct = ref.read(savedProductListStateProvider).indexWhere((e) => e.id == id);
-        final currentList = ref.read(savedProductListStateProvider);
-        if (indexOfProduct > -1) {
-          currentList.removeAt(indexOfProduct);
-          ref.read(savedProductListStateProvider.notifier).state = [...currentList];
-        } else {
-          ref.read(savedProductListStateProvider.notifier).state = [
-            ...currentList,
-            ProductModel(
-              id: id,
-              name: productName,
-              imageUrl: imageUrl,
-              price: price,
-            )
-          ];
-        }
-      },
-      minSize: 20,
-      padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-      child: FaIcon(
-        isFavorite ? FontAwesomeIcons.solidHeart : FontAwesomeIcons.heart,
-        color: Colors.red,
       ),
     );
   }
---
+++ lib/presentation/common/widgets/saved_button.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../../domain/models/product_model.dart';
+import '../../../domain/states/saved_product_list_provider_state_provider.dart';
+
+class SaveButton extends ConsumerWidget {
+  const SaveButton({
+    super.key,
+    required this.id,
+    required this.productName,
+    required this.imageUrl,
+    required this.price,
+  });
+
+  final String id;
+  final String productName;
+  final String imageUrl;
+  final double price;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isFavorite = ref.watch(savedProductListStateProvider).indexWhere((e) => e.id == id) > -1;
+
+    return CupertinoButton(
+      onPressed: () {
+        final indexOfProduct = ref.read(savedProductListStateProvider).indexWhere((e) => e.id == id);
+        final currentList = ref.read(savedProductListStateProvider);
+        if (indexOfProduct > -1) {
+          currentList.removeAt(indexOfProduct);
+          ref.read(savedProductListStateProvider.notifier).state = [...currentList];
+        } else {
+          ref.read(savedProductListStateProvider.notifier).state = [
+            ...currentList,
+            ProductModel(
+              id: id,
+              name: productName,
+              imageUrl: imageUrl,
+              price: price,
+            )
+          ];
+        }
+      },
+      minSize: 20,
+      padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+      child: FaIcon(
+        isFavorite ? FontAwesomeIcons.solidHeart : FontAwesomeIcons.heart,
+        color: Colors.red,
+      ),
+    );
+  }
+}
